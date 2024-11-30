010a4ed9bd9eac481feb7b145c5cdece89a0484a
Niraj Shaha <86703545+NirajShaha@users.noreply.github.com>
2024-11-30 19:32:26 +0000

    🧰 Editor tools like undo, redo, bold. unset, underline implmentation

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,7 +1,20 @@
 "use client";
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
-import { LucideIcon, Undo2Icon } from "lucide-react";
+import {
+  BoldIcon,
+  ItalicIcon,
+  ListTodoIcon,
+  LucideIcon,
+  MessageSquarePlusIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormattingIcon,
+  SpellCheckIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from "lucide-react";
 
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -28,26 +41,109 @@ const ToolbarButton = ({
 };
 
 export const ToolBar = () => {
-  const {editor} = useEditorStore();
-  console.log("Toolbar editor: ", {editor});
-  
-    const sections: {
-      label: string;
-      icon: LucideIcon;
-      onClick: () => void;
-      isActive?: boolean;
-    }[][] = [
-      [
-        {
-          label:"Undo",
-          icon: Undo2Icon,
-          onClick: () => editor?.chain().focus().undo().run(),
-        }
-      ]
+  const { editor } = useEditorStore();
+  console.log("Toolbar editor: ", { editor });
+
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
+    [
+      {
+        label: "Undo",
+        icon: Undo2Icon,
+        onClick: () => editor?.chain().focus().undo().run(),
+      },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: "Spell Check",
+        icon: SpellCheckIcon,
+        onClick: () => {
+          console.log("Spell Check");
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "true" : "false"
+          );
+        },
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        isActive: editor?.isActive("bold"),
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
+      {
+        label: "Italic",
+        icon: ItalicIcon,
+        isActive: editor?.isActive("italic"),
+        onClick: () => editor?.chain().focus().toggleItalic().run(),
+      },
+      {
+        label: "Underline",
+        icon: UnderlineIcon,
+        isActive: editor?.isActive("underline"),
+        onClick: () => editor?.chain().focus().toggleUnderline().run(),
+      },
+    ],
+    [
+      {
+        label: "Comments",
+        icon: MessageSquarePlusIcon,
+        onClick: () => console.log("comment"),
+        isActive: false,
+      },
+      {
+        label: "List Todo",
+        icon: ListTodoIcon,
+        onClick: () => editor?.chain().focus().toggleTaskList().run(),
+        isActive: editor?.isActive("taskList"),
+      },
+      {
+        label: "Remove Formatting",
+        icon: RemoveFormattingIcon,
+        onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+        isActive: editor?.isActive("removeFormatting"),
+      },
     ]
+  ];
   return (
     <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
-      {sections[0].map((item)=>(
+      {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/*TODO: Font family*/} 
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/*TODO: Heading*/}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/*TODO: Font size*/}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[1].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      {/*TODO: Text Color*/}
+      {/*TODO: Highlight color*/}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/*TODO: Link*/}
+      {/*TODO: Image*/}
+      {/*TODO: Align*/}
+      {/*TODO: Line Height*/}
+      {/*TODO: List*/}
+      {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
     </div>
