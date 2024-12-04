e80ca805a4680ed055cbc464cdadbce4abb3e812
Niraj Shaha <86703545+NirajShaha@users.noreply.github.com>
2024-12-04 18:41:11 +0000

    :sparkles: setup convex database

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,19 @@
+"use client"
+
 import React from 'react'
 import { Navbar } from './navbar'
-import Link from 'next/link';
 import { TemplateGallery } from './template-gallery';
+import { api } from '../../../convex/_generated/api';
+import { useQuery } from 'convex/react';
 const Home: React.FC = () => {
+
+  const documents = useQuery(api.documents.get);
+
+  if(documents === undefined) {
+    return (
+      <p>Loading</p>
+    )
+  }
   return (
     <div className="min-h-screen flex flex-col">
       <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white p-6'>
@@ -10,6 +21,9 @@ const Home: React.FC = () => {
       </div>
       <div className='mt-16'>
         <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
